07ca32bc6ccf96a84660f25fdde7c4034d04302b
wofn <siny10188@naver.com>
2022-12-03 00:13:53 +0900

    Update init.js

--- src/init.js
+++ src/init.js
@@ -6,7 +6,7 @@ import "./models/User";
 import "./models/Comment";
 import app from "./server";
 
-const PORT = 4000;
+const PORT = process.env.PORT;
 
 const hadleListening = () =>
   console.log(`✅server listening on port http://localhost:${PORT}`);
